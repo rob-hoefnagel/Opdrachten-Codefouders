67bad40fcdfda87bd9973886f199a87da36eb558
rob-hoefnagel <93723902+rob-hoefnagel@users.noreply.github.com>
2022-03-31 11:54:31 +0200

    Add files via upload

---
+++ Pogingen.java
@@ -0,0 +1,12 @@
+public class Pogingen {
+
+    public static void main(String[]args){
+
+        String eersteString = "Po";
+        String tweedeString = "ging";
+        String derdeString = "en";
+        System.out.println(eersteString + tweedeString + derdeString);
+    }
+
+
+}
